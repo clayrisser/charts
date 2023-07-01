03988da83122313e41d5ad67425d67249f155a8e
Clay Risser <clayrisser@gmail.com>
2023-07-01 04:32:07 -0500

    🚑 fix: Update bootstrap file with new version and checksum
    The bootstrap file `.mkpm/.bootstrap.mk` has been updated with a new version and checksum. The version has been changed to use the Git LFS specification, and the checksum has been updated to `07a74c3660a2b7e55e958bfac55bebba651b82da2b4d9034cb06b4f0a7d0afdc`. The file size remains the same at 16452 bytes.

--- .mkpm/.bootstrap.mk
+++ .mkpm/.bootstrap.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8eb4944e1d8a33cd2d20ad4b7b50bf6e666084367c933c17406e02b2cc90bc71
-size 16450
+oid sha256:07a74c3660a2b7e55e958bfac55bebba651b82da2b4d9034cb06b4f0a7d0afdc
+size 16452
