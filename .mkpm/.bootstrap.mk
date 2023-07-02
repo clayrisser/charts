db96b58f3249b63a2841ff5543b6052c9ce6865d
Clay Risser <clayrisser@gmail.com>
2023-07-02 01:30:00 -0500

    🔧 chore: Update .mkpm/.bootstrap.mk and beta/aws-creds/questions.yaml
    - Updated .mkpm/.bootstrap.mk with new oid sha256:0634d4729681f01e488a4680b048e9638c5e6e0ad4d13da2a60a03987dbce849 and size 16423.
    - Changed category from Server to AWS in beta/aws-creds/questions.yaml.

--- .mkpm/.bootstrap.mk
+++ .mkpm/.bootstrap.mk
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:07a74c3660a2b7e55e958bfac55bebba651b82da2b4d9034cb06b4f0a7d0afdc
-size 16452
+oid sha256:0634d4729681f01e488a4680b048e9638c5e6e0ad4d13da2a60a03987dbce849
+size 16423
